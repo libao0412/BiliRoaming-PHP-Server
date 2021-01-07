b5342a1e40af087b0ff455e11667efe27cbb02e2
david082321 <7046617+david082321@users.noreply.github.com>
2021-01-08 01:46:13 +0800

    Fisrt Version

---
+++ intl/gateway/v2/app/subtitle/index.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+// 模块请求都会带上X-From-Biliroaming的请求头，为了防止被盗用，可以加上请求头判断
+$headerStringValue = $_SERVER['HTTP_X_FROM_BILIROAMING'];
+if ($headerStringValue==""){
+    header('Content-Type: application/json; charset=utf-8');
+    echo '{"code":-10403,"message":"抱歉您所在地区不可观看"}';
+    exit();
+}
+
+// 转发到b站服务器
+$url = "https://app.global.bilibili.com/intl/gateway/v2/app/subtitle?".$_SERVER['QUERY_STRING'];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+$output = curl_exec($ch);
+curl_close($ch);
+print($output);
+?>
